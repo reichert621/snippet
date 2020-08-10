0a1f8fd6a01929dc9e729f1dec1de823c36cc05d
reichert621 <reichertjalex@gmail.com>
2020-08-10 14:37:17 -0400

    Fix metadata

--- src/api.ts
+++ src/api.ts
@@ -3,9 +3,9 @@ import {DEFAULT_BASE_URL} from './config';
 import {now} from './utils';
 
 export type CustomerMetadata = {
-  name: string;
-  email: string;
-  external_id: string;
+  name?: string;
+  email?: string;
+  external_id?: string;
   // TODO: include browser info
 };
 
