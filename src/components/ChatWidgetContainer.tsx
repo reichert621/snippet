eddbdc491e94ac8e99b9bb4fa873f0164d6d0579
reichert621 <reichertjalex@gmail.com>
2020-12-09 21:40:08 -0500

    Update latest

--- src/components/ChatWidgetContainer.tsx
+++ src/components/ChatWidgetContainer.tsx
@@ -10,7 +10,7 @@ import {
   fetchWidgetSettings,
   updateWidgetSettingsMetadata,
 } from '../api';
-import {WidgetConfig, noop} from '../utils';
+import {WidgetConfig, isValidUuid, noop} from '../utils';
 import getThemeConfig from '../theme';
 import store from '../storage';
 import {isDev} from '../config';
@@ -63,6 +63,7 @@ export type SharedProps = {
   greeting?: string;
   customer?: CustomerMetadata | null;
   newMessagePlaceholder?: string;
+  emailInputPlaceholder?: string;
   agentAvailableText?: string;
   agentUnavailableText?: string;
   showAgentAvailability?: boolean;
@@ -74,6 +75,10 @@ export type SharedProps = {
   onChatClosed?: () => void;
   onMessageSent?: (message: Message) => void;
   onMessageReceived?: (message: Message) => void;
+  // TODO: how should we name these?
+  setDefaultTitle?: (settings: WidgetSettings) => string | Promise<string>;
+  setDefaultSubtitle?: (settings: WidgetSettings) => string | Promise<string>;
+  setDefaultGreeting?: (settings: WidgetSettings) => string | Promise<string>;
 };
 
 type Props = SharedProps & {
@@ -110,6 +115,14 @@ class ChatWidgetContainer extends React.Component<Props, State> {
 
     if (!props.accountId) {
       throw new Error('An `accountId` is required to run the Papercups chat!');
+    } else if (!isValidUuid(props.accountId)) {
+      console.error(
+        `The \`accountId\` must be a valid UUID. (Received invalid \`accountId\`: ${props.accountId})`
+      );
+      console.error(
+        `If you're missing a Papercups \`accountId\`, you can get one by signing up for a free account at https://app.papercups.io/register`
+      );
+      throw new Error(`Invalid \`accountId\`: ${props.accountId}`);
     }
 
     this.state = {
@@ -128,12 +141,10 @@ class ChatWidgetContainer extends React.Component<Props, State> {
     const settings = await this.fetchWidgetSettings();
     const {
       accountId,
-      title,
-      subtitle,
       primaryColor,
       baseUrl,
-      greeting,
       newMessagePlaceholder,
+      emailInputPlaceholder,
       agentAvailableText,
       agentUnavailableText,
       showAgentAvailability,
@@ -158,12 +169,14 @@ class ChatWidgetContainer extends React.Component<Props, State> {
       baseUrl,
       agentAvailableText,
       agentUnavailableText,
-      title: title || settings.title,
-      subtitle: subtitle || settings.subtitle,
+      title: await this.getDefaultTitle(settings),
+      subtitle: await this.getDefaultSubtitle(settings),
       primaryColor: primaryColor || settings.color,
-      greeting: greeting || settings.greeting,
+      greeting: await this.getDefaultGreeting(settings),
       newMessagePlaceholder:
         newMessagePlaceholder || settings.new_message_placeholder,
+      emailInputPlaceholder:
+        emailInputPlaceholder || settings.email_input_placeholder,
       companyName: settings?.account?.company_name,
       requireEmailUpfront: requireEmailUpfront ? 1 : 0,
       showAgentAvailability: showAgentAvailability ? 1 : 0,
@@ -199,6 +212,7 @@ class ChatWidgetContainer extends React.Component<Props, State> {
       baseUrl,
       greeting,
       newMessagePlaceholder,
+      emailInputPlaceholder,
     } = this.props;
     const current = [
       accountId,
@@ -208,6 +222,7 @@ class ChatWidgetContainer extends React.Component<Props, State> {
       baseUrl,
       greeting,
       newMessagePlaceholder,
+      emailInputPlaceholder,
     ];
     const prev = [
       prevProps.accountId,
@@ -217,6 +232,7 @@ class ChatWidgetContainer extends React.Component<Props, State> {
       prevProps.baseUrl,
       prevProps.greeting,
       prevProps.newMessagePlaceholder,
+      prevProps.emailInputPlaceholder,
     ];
     const shouldUpdate = current.some((value, idx) => {
       return value !== prev[idx];
@@ -234,9 +250,40 @@ class ChatWidgetContainer extends React.Component<Props, State> {
         baseUrl,
         greeting,
         newMessagePlaceholder,
+        emailInputPlaceholder,
       });
     }
   }
+
+  getDefaultTitle = async (settings: WidgetSettings) => {
+    const {title, setDefaultTitle} = this.props;
+
+    if (setDefaultTitle && typeof setDefaultTitle === 'function') {
+      return setDefaultTitle(settings);
+    } else {
+      return title || settings.title;
+    }
+  };
+
+  getDefaultSubtitle = async (settings: WidgetSettings) => {
+    const {subtitle, setDefaultSubtitle} = this.props;
+
+    if (setDefaultSubtitle && typeof setDefaultSubtitle === 'function') {
+      return setDefaultSubtitle(settings);
+    } else {
+      return subtitle || settings.subtitle;
+    }
+  };
+
+  getDefaultGreeting = async (settings: WidgetSettings) => {
+    const {greeting, setDefaultGreeting} = this.props;
+
+    if (setDefaultGreeting && typeof setDefaultGreeting === 'function') {
+      return setDefaultGreeting(settings);
+    } else {
+      return greeting || settings.greeting;
+    }
+  };
 
   setIframeRef = (el: HTMLIFrameElement) => {
     this.iframeRef = el;
@@ -271,7 +318,7 @@ class ChatWidgetContainer extends React.Component<Props, State> {
     this.logger.debug('Updated customer ID:', customerId);
   };
 
-  fetchWidgetSettings = () => {
+  fetchWidgetSettings = async (): Promise<WidgetSettings> => {
     const {accountId, baseUrl} = this.props;
     const empty = {} as WidgetSettings;
 
--- src/utils.ts
+++ src/utils.ts
@@ -6,6 +6,7 @@ export type WidgetConfig = {
   primaryColor?: string;
   greeting?: string;
   newMessagePlaceholder?: string;
+  emailInputPlaceholder?: string;
   companyName?: string;
   agentAvailableText?: string;
   agentUnavailableText?: string;
@@ -19,6 +20,16 @@ export type WidgetConfig = {
 };
 
 export function noop() {}
+
+export function isValidUuid(id: any) {
+  if (!id || typeof id !== 'string' || !id.length) {
+    return false;
+  }
+
+  const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+  return regex.test(id);
+}
 
 // TODO: handle this on the server instead
 export function now() {
