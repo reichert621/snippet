312d70b3904f34c7464e71603bf75ebc0aca03b5
reichert621 <reichertjalex@gmail.com>
2021-07-26 14:35:48 -0700

    Test using IIFE

--- src/index.tsx
+++ src/index.tsx
@@ -4,82 +4,91 @@ import ChatWidget from './components/ChatWidget';
 
 // TODO: there's probably a more elegant way to do this...
 
-const noop = () => {};
-const w = window as any;
-const config = (w.Papercups && w.Papercups.config) || {};
-const {
-  accountId,
-  title,
-  subtitle,
-  newMessagePlaceholder,
-  emailInputPlaceholder,
-  newMessagesNotificationText,
-  primaryColor,
-  greeting,
-  awayMessage,
-  customer,
-  baseUrl,
-  iframeUrlOverride,
-  customIconUrl,
-  agentAvailableText,
-  agentUnavailableText,
-  showAgentAvailability,
-  iconVariant = 'outlined',
-  hideToggleButton = false,
-  requireEmailUpfront = false,
-  defaultIsOpen = false,
-  isOpenByDefault = false,
-  persistOpenState = false,
-  position = 'right',
-  styles = {},
-  onChatLoaded = noop,
-  onChatOpened = noop,
-  onChatClosed = noop,
-  onMessageSent = noop,
-  onMessageReceived = noop,
-} = config;
+(function (w: any) {
+  const noop = () => {};
+  const config = (w.Papercups && w.Papercups.config) || {};
+  const el = document.createElement('div');
+  el.id = 'PapercupsChatWidget';
+  document.body.appendChild(el);
 
-if (!accountId) {
-  throw new Error(
-    'An account token is required to render the Papercups chat widget!'
-  );
-}
+  const init = (overrides = {}) => {
+    const params = {...config, ...overrides};
+    const {
+      accountId,
+      title,
+      subtitle,
+      newMessagePlaceholder,
+      emailInputPlaceholder,
+      newMessagesNotificationText,
+      primaryColor,
+      greeting,
+      awayMessage,
+      customer,
+      baseUrl,
+      iframeUrlOverride,
+      customIconUrl,
+      agentAvailableText,
+      agentUnavailableText,
+      showAgentAvailability,
+      iconVariant = 'outlined',
+      hideToggleButton = false,
+      requireEmailUpfront = false,
+      defaultIsOpen = false,
+      isOpenByDefault = false,
+      persistOpenState = false,
+      popUpInitialMessage = false,
+      position = 'right',
+      styles = {},
+      onChatLoaded = noop,
+      onChatOpened = noop,
+      onChatClosed = noop,
+      onMessageSent = noop,
+      onMessageReceived = noop,
+    } = params;
 
-const el = document.createElement('div');
-el.id = 'PapercupsChatWidget';
-document.body.appendChild(el);
+    if (!accountId) {
+      throw new Error(
+        'An account token is required to render the Papercups chat widget!'
+      );
+    }
 
-ReactDOM.render(
-  <ChatWidget
-    accountId={accountId}
-    title={title}
-    subtitle={subtitle}
-    newMessagePlaceholder={newMessagePlaceholder}
-    emailInputPlaceholder={emailInputPlaceholder}
-    newMessagesNotificationText={newMessagesNotificationText}
-    primaryColor={primaryColor}
-    greeting={greeting}
-    awayMessage={awayMessage}
-    customer={customer}
-    baseUrl={baseUrl}
-    iframeUrlOverride={iframeUrlOverride}
-    customIconUrl={customIconUrl}
-    agentAvailableText={agentAvailableText}
-    agentUnavailableText={agentUnavailableText}
-    showAgentAvailability={showAgentAvailability}
-    requireEmailUpfront={requireEmailUpfront}
-    defaultIsOpen={defaultIsOpen}
-    isOpenByDefault={isOpenByDefault}
-    persistOpenState={persistOpenState}
-    iconVariant={iconVariant}
-    hideToggleButton={hideToggleButton}
-    styles={styles}
-    position={position}
-    onChatLoaded={onChatLoaded}
-    onChatOpened={onChatOpened}
-    onChatClosed={onChatClosed}
-    onMessageSent={onMessageSent}
-    onMessageReceived={onMessageReceived}
-  />,
-  document.getElementById('PapercupsChatWidget')
-);
+    ReactDOM.render(
+      <ChatWidget
+        accountId={accountId}
+        title={title}
+        subtitle={subtitle}
+        newMessagePlaceholder={newMessagePlaceholder}
+        emailInputPlaceholder={emailInputPlaceholder}
+        newMessagesNotificationText={newMessagesNotificationText}
+        primaryColor={primaryColor}
+        greeting={greeting}
+        awayMessage={awayMessage}
+        customer={customer}
+        baseUrl={baseUrl}
+        iframeUrlOverride={iframeUrlOverride}
+        customIconUrl={customIconUrl}
+        agentAvailableText={agentAvailableText}
+        agentUnavailableText={agentUnavailableText}
+        showAgentAvailability={showAgentAvailability}
+        requireEmailUpfront={requireEmailUpfront}
+        defaultIsOpen={defaultIsOpen}
+        isOpenByDefault={isOpenByDefault}
+        persistOpenState={persistOpenState}
+        popUpInitialMessage={popUpInitialMessage}
+        iconVariant={iconVariant}
+        hideToggleButton={hideToggleButton}
+        styles={styles}
+        position={position}
+        onChatLoaded={onChatLoaded}
+        onChatOpened={onChatOpened}
+        onChatClosed={onChatClosed}
+        onMessageSent={onMessageSent}
+        onMessageReceived={onMessageReceived}
+      />,
+      document.getElementById('PapercupsChatWidget')
+    );
+  };
+
+  w.Papercups.init = init;
+  w.Papercups.init();
+})(window);
